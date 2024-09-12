60b048a927541e22b11f3cfb27f92a1153c716e4
Prunel Mel <seyive.awaya@gmail.com>
2024-09-12 09:26:25 +0100

    updating nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,26 @@
-
+import react from '../assets/react.svg'
 const Navbar = () => {
   return (
     <nav className='bg-indigo-700 border-b border-indigo-500'>
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='flex h-20 items-center justify-between'>
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-            <span className='hidden md:block text-white text-2xl font-bold ml-2'>
-            React Jobs
-            </span>
+            {/* <span className='hidden md:block text-white text-2xl font-bold ml-2'>
+              React Jobs
+            </span> */}
+            <img src={react} alt="" />
             <div className='md:ml-auto'>
               <div className='flex space-x-4'>
-                <a className="text-white hover:bg-black p-2">Home</a>
-                <a className="text-white hover:bg-black p-2">Jobs</a>
-                <a className="text-white hover:bg-black p-2">Add Jobs</a>
+                <div className='flex items-center justify-center border border-transparent rounded hover:bg-black'>
+                  <a className="text-white p-2">Home</a>
+                </div>
+                <div className='flex items-center justify-center border border-transparent rounded hover:border-black'>
+                  <a className="text-white hover:bg-black p-2">Jobs</a>
+
+                </div>
+                <div className='flex items-center justify-center border border-transparent rounded hover:border-black'>
+                  <a className="text-white hover:bg-black p-2">Add Jobs</a> 
+                </div>
                 
               </div>
             </div>
